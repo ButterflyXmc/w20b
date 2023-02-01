f7f0fc973b2a71d565689a635d62108c13790a99
mahic <mahic@domain.com>
2023-02-01 00:03:16 -0700

    First commit

---
+++ app.py
@@ -0,0 +1,13 @@
+import mariadb
+import dbcreds
+
+
+conn = mariadb.connect(
+                            user = dbcreds.user,
+                            password = dbcreds.password,
+                            host = dbcreds.host,
+                            port = dbcreds.port,
+                            database = dbcreds.database,
+                            autocommit = True
+                            )
+cursor = conn.cursor()
